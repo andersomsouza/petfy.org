1edb27f2c778ed512970845acf3b3ad83d77a21f
vhsoares <vsbbbeico@gmail.com>
2017-10-28 23:48:15 -0200

    mentoria

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>Petotas</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+//mix.js('resources/assets/js/app.js', 'public/js')
+//   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.stylus('resources/assets/stylus/template.styl','public/css/template.css',{
+    use: [
+        require('rupture')()
+    ]
+});
