e2f5f4c5a8aea7d1860b4f8bc8cb47384dad50dd
Anderson Souza <andersom_jag@hotmail.com>
2017-10-29 11:09:41 -0200

    corrigido entidades

--- database/seeds/UsersTableSeed.php
+++ database/seeds/UsersTableSeed.php
@@ -17,6 +17,27 @@ class UsersTableSeed extends Seeder
         $user->name = "Anderson";
         $user->email = "anderson@garten.com.br";
         $user->password = Hash::make('123456');
+        $user->endereco = "Rua Bernardo José de Souza, 434";
+        $user->contato = "(53) 9 8112 9428";
+        $user->obs = "";
+        $user->save();
+
+        $user = new User();
+        $user->name = "Gabriel";
+        $user->email = "gdmramos@inf.ufpel.edu.br";
+        $user->password = Hash::make('123456');
+        $user->endereco = "Rua Bernardo Independencia, 124";
+        $user->contato = "(53) 9 8322 9658";
+        $user->obs = "";
+
+        $user->save();
+        $user = new User();
+        $user->name = "Matheus Eduardo";
+        $user->email = "freitag.matheus@gmail.com";
+        $user->password = Hash::make('123456');
+        $user->endereco = "Rua Alameda do Café, 422";
+        $user->contato = "(53) 9 9123 9458";
+        $user->obs = "";
         $user->save();
 
 
