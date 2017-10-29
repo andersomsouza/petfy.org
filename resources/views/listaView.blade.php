e229b45057a0af7e9458388de353d7030e584d68
vhsoares <vsbbbeico@gmail.com>
2017-10-29 01:42:53 -0200

    push que nao foi

---
+++ resources/views/listaView.blade.php
@@ -0,0 +1,38 @@
+<div class="resultadoPesquisaAnimal">
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+    <div class="animal">
+        <img src="http://placehold.it/150x150" alt="">
+        <p>(Sexo), esperando desde (data)</p>
+    </div>
+</div>
