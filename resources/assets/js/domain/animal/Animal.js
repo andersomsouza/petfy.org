43e85bdd40baf9bd0554ee3e27bcf190635a8606
Andersom Souza <andersom.eugenio@gmail.com>
2018-01-25 19:53:54 -0200

    adicionada primeiras pros do Animal + get/setts

--- resources/assets/js/domain/animal/Animal.js
+++ resources/assets/js/domain/animal/Animal.js
@@ -1,4 +1,39 @@
 export default class Animal {
-    constructor(){}
+    constructor(descricao = "", foto = "", idade = "", porte = ""){
+        this._descricao = descricao;
+        this._foto = foto;
+        this._idade = idade;
+        this._porte = porte;
+    }
 
+
+    get foto(){
+        return this._foto;
+    }
+    get descricao(){
+        return this._descricao;
+    }
+    get idade(){
+        return this._idade;
+    }
+    get porte(){
+        return this._porte;
+    }
+
+
+    set idade(value) {
+        this._idade = value;
+    }
+
+    set descricao(value) {
+        this._descricao = value;
+    }
+
+    set foto(value) {
+        this._foto = value;
+    }
+
+    set porte(value) {
+        this._porte = value;
+    }
 }
